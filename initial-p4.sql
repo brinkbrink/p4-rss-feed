cd54a7a5d7bb2d29ac94c80375b8b5899c31f6f2
Ryan A. Novelli <ryan.a.novelli@gmail.com>
2022-03-08 13:17:59 -0800

    Updates music feeds

--- initial-p4.sql
+++ initial-p4.sql
@@ -33,9 +33,9 @@ INSERT INTO winter2022_feeds VALUES (NULL,1,1,'Chanterelles','https://news.googl
 INSERT INTO winter2022_feeds VALUES (NULL,1,1,'Morels','https://news.google.com/rss/search?q=morel&hl=en-US&gl=US&ceid=US:en',NOW(),NOW());
 INSERT INTO winter2022_feeds VALUES (NULL,1,1,'Psilocybe','https://news.google.com/rss/search?q=psilocybe&hl=en-US&gl=US&ceid=US:en',NOW(),NOW());
 
-INSERT INTO winter2022_feeds VALUES (NULL,2,1,'Jazz','https://news.google.com/rss/search?q=jazz&hl=en-US&gl=US&ceid=US:en',NOW(),NOW());
-INSERT INTO winter2022_feeds VALUES (NULL,2,1,'Metal','https://news.google.com/rss/search?q=metal&hl=en-US&gl=US&ceid=US:en',NOW(),NOW());
-INSERT INTO winter2022_feeds VALUES (NULL,2,1,'Hip Hop','https://news.google.com/rss/search?q=hiphop&hl=en-US&gl=US&ceid=US:en',NOW(),NOW());
+INSERT INTO winter2022_feeds VALUES (NULL,2,1,'Jazz','https://londonjazznews.com/feed/?alt=rss',NOW(),NOW());
+INSERT INTO winter2022_feeds VALUES (NULL,2,1,'Rock & Roll','https://garagehangover.com/feed',NOW(),NOW());
+INSERT INTO winter2022_feeds VALUES (NULL,2,1,'Soul','https://www.funkmysoul.gr/feed',NOW(),NOW());
 
 INSERT INTO winter2022_feeds VALUES (NULL,3,1,'New Yorker','https://www.newyorker.com/feed/tech',NOW(),NOW());
 INSERT INTO winter2022_feeds VALUES (NULL,3,1,'Wired','https://www.wired.com/feed/category/gear/latest/rss',NOW(),NOW());
